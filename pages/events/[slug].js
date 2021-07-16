ce2c550fcdf1f5f0fec68147dec9c25a47a5ccf0
Ryan <ryanjay333@gmail.com>
2021-07-16 11:21:58 -0400

    created Layout, Header and Footer

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,7 +1,15 @@
+import { useRouter } from 'next/router';
+import Layout from '../components/Layout';
+
 export default function EventPage() {
+  const router = useRouter();
+  const slug = router.query.slug;
+
   return (
-    <div>
+    <Layout title={`${slug} | DJ Events`}>
       <h1>Event Page</h1>
-    </div>
+      <h3>{slug}</h3>
+      <button onClick={() => router.push('/')}>Click</button>
+    </Layout>
   );
 }
